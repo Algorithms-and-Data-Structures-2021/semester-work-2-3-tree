acc3939262999cef981bc686400bfa6429958662
Roman Browin <roman.browin@yandex.ru>
2021-04-25 22:09:32 +0400

    add examples

---
+++ examples/example.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Marcusfam on 25.04.2021.
+//
+
+#include <iostream>
+
+#include "data_structure.hpp"
+#include "traversal_algorithm.h"
+
+using namespace std;
+using namespace itis;
+
+int main() {
+
+  // здесь проводились тесты дерева
+  TwoThreeTree tree;
+
+  // делаем вставку 4 элементов в пустое дерево(вставляем 1, 2, 3, 4)
+  for (int i = 1; i < 5; i++) {
+    tree.Insert(i);
+  }
+
+  // ищем в дереве вершины с ключами 2 и 6, если не нащлось, то вернет 0
+  cout << tree.Search(2) << endl;
+  cout << tree.Search(6) << endl;
+
+  // удаляем 2 элемента, если элемент не найден, то ничего не произойдет
+  tree.Remove(2);
+  tree.Remove(6);
+
+  // печатаем наше дерево (учтите, что у кажой вершины может быть два ключа, если на втором месте стоит ноль,
+  // значит второй ключ отсутствует
+  TraversalAlgorithm *algorithm = new BreadthFirstTraversalAlgorithm;
+
+  std::cout << "Level-order (BFS): ";
+  tree.Traverse(*algorithm);
+  std::cout << std::endl;
+
+  delete[] algorithm;
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,33 +1,11 @@
 #include <iostream>
 
 #include "data_structure.hpp"
-#include "traversal_algorithm.h"
 
 using namespace itis;
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  // здесь проводились тесты дерева
-  TwoThreeTree tree;
-
-  // делаем вставку 4 элементов в пустое дерево
-  for(int i = 1; i < 5; i++){
-    tree.Insert(i);
-  }
-
-  // удаляем 3 элемента
-  tree.Remove(2);
-  tree.Remove(4);
-  tree.Remove(3);
-
-  // печатаем наше дерево(правда в нем всего одна вершина)
-  TraversalAlgorithm *algorithm = new BreadthFirstTraversalAlgorithm;
-
-  std::cout << "Level-order (BFS): ";
-  tree.Traverse(*algorithm);
-  std::cout << std::endl;
-
-  delete [] algorithm;
 
   for (int index = 0; index < argc; index++) {
     cout << index << ": " << argv[index] << endl;
